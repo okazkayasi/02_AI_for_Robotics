282c3a3e21656d8131efdbb1b15952f4e2b68a33
okazkayasi <okazkayasi@outlook.com>
2018-06-30 19:56:42 +0300

    Problem Set 5 and Quizzes of the Class

--- 08_Quiz_Robot_Motion/pathSmoothing.py
+++ 08_Quiz_Robot_Motion/pathSmoothing.py
@@ -33,34 +33,26 @@ path = [[0, 0],
         [4, 3],
         [4, 4]]
 
-def smooth(path, weight_data = 0.5, weight_smooth = 0.1, tolerance = 0.000001):
+def smooth(path, weight_data = 0.0, weight_smooth = 0.1, tolerance = 0.000001):
 
     # Make a deep copy of path into newpath
     newpath = deepcopy(path)
 
-    #######################
-    ### ENTER CODE HERE ###
-    #######################
     # keep updating the newpath until the TOTAL change is smaller than tolerence
     converge = False
     while not converge:
-        total_change = 0
-        for pos in range(len(path)-1):
-            if pos != 0:  
-                alpha = weight_data * (newpath[pos][0] - path[pos][0])
-                beta = weight_smooth * (newpath[pos+1][0] + newpath[pos-1][0] - 2*newpath[pos][0])
-                hold = newpath[pos][0] + alpha + beta               
-                total_change += abs(hold - newpath[pos][0])
-                print 'x coordinate change: ', total_change
-                newpath[pos][0] = hold
-                alpha = weight_data * (newpath[pos][1] - path[pos][1])
-                beta = weight_smooth * (newpath[pos+1][1] + newpath[pos-1][1] - 2*newpath[pos][1])
-                hold = newpath[pos][1] + alpha + beta
-                print 'y coordinate change: ', abs(hold - newpath[pos][1])
-                total_change += abs(hold - newpath[pos][1])
-                newpath[pos][1] = hold  
-        print total_change     
-        printpaths(path, newpath)
+        total_change = 0.0
+        for pos in range(1, len(path)-1):    
+            alpha = weight_data * (path[pos][0] - newpath[pos][0])
+            beta = weight_smooth * (newpath[pos+1][0] + newpath[pos-1][0] - 2*newpath[pos][0])
+            hold = newpath[pos][0] + alpha + beta               
+            total_change += abs(hold - newpath[pos][0])
+            newpath[pos][0] = hold
+            alpha = weight_data * (path[pos][1] - newpath[pos][1])
+            beta = weight_smooth * (newpath[pos+1][1] + newpath[pos-1][1] - 2*newpath[pos][1])
+            hold = newpath[pos][1] + alpha + beta
+            total_change += abs(hold - newpath[pos][1])
+            newpath[pos][1] = hold      
         if total_change < tolerance:
             converge = True
     
